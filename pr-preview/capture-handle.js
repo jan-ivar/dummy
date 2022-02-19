6c1b8a2a03b3737addbb33a146c5c71ccc34d0cf
Jan-Ivar Bruaroey <wingboy@gmail.com>
2022-02-18 21:43:42 -0500

    Add preview of capture handle PR 15

---
+++ pr-preview/capture-handle.js
@@ -0,0 +1,18 @@
+var respecConfig = {
+  group: "webrtc",
+  specStatus: "unofficial",
+  github: {
+    repoURL: "https://github.com/wicg/capture-handle/",
+    branch: "master",
+  },
+  editors: [
+    {
+      name: "Elad Alon",
+      email: "eladalon@google.com",
+      company: "Google",
+    },
+  ],
+  xref: ["html", "infra", "permissions", "permissions-policy", "dom", "mediacapture-streams", "webidl", "screen-capture"],
+  shortName: "capture-handle",
+  subjectPrefix: "[capture-handle]",
+};
